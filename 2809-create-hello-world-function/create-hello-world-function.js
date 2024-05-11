54b50db29683d1fa5576d6c8ba63d9185cf9f05d
Anish T <85789404+Anish6964@users.noreply.github.com>
2024-05-11 16:28:44 +0530

    Time: 53 ms (37.77%) | Memory: 48.9 MB (23.43%) - LeetSync

--- 2809-create-hello-world-function/create-hello-world-function.js
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -1,16 +1,8 @@
 /**
  * @return {Function}
  */
-var createHelloWorld = function() {
-    
-    return ()=> "Hello World"
-   
-
-        
-    
-};
+var createHelloWorld = function() {return ()=> "Hello World"   }
 
 
-const f = createHelloWorld();
-f(); 
+
  
