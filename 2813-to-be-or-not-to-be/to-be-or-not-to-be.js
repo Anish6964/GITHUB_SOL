0ad305a6092302662270a2239e667c146fcf9bb8
Anish T <85789404+Anish6964@users.noreply.github.com>
2024-08-25 22:36:29 +0530

    Time: 57 ms (26.07%) | Memory: 48.8 MB (47.87%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -2,27 +2,27 @@
  * @param {string} val
  * @return {Object}
  */
-var expect = function(val) {
-    
+var expect = function (val) {
+
     return {
 
-     toBe : (value) => {
-        if (value === val) {
-            return true;
+        toBe: (value) => {
+            if (value === val) {
+                return true;
+            }
+            else {
+                throw new Error("Not Equal")
+            }
+        },
+
+        notToBe: (value) => {
+            if (value !== val) {
+                return true;
+            }
+            else {
+                throw new Error("Equal")
+            }
         }
-        else{
-            throw new Error("Not Equal")
-        }
-    },
-    
-     notToBe : (value) => {
-        if(value !== val ){
-            return true;
-        }
-        else{
-            throw new Error("Equal")
-        }
-    }
 
     }
 };
