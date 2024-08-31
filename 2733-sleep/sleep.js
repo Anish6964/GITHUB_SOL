eac20552679388da2d76a78dab99a75a6da46ca3
Anish T <85789404+Anish6964@users.noreply.github.com>
2024-08-31 16:48:42 +0530

    Time: 48 ms (79.36%) | Memory: 48.9 MB (47.05%) - LeetSync

---
+++ 2733-sleep/sleep.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+
+    return new Promise((resolve,reject)=> {
+        setTimeout(()=>{
+            resolve("something")
+
+        }, millis);
+    })
+    
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
