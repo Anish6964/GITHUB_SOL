0a692099f5e4b69fd4ecb5ce42505d98b9bd0b20
Anish T <85789404+Anish6964@users.noreply.github.com>
2024-08-31 15:29:09 +0530

    Time: 47 ms (83.47%) | Memory: 49 MB (36.87%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+
+    let counter=0
+    
+    return function(...args){
+
+        counter= counter + 1
+
+        if(counter === 1){
+            return fn(...args)
+        }
+
+       
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
